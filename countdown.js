bf4008aa347dcebe371390613a284662b90c1721
Ara Garrido <94240265+invitacionesweb@users.noreply.github.com>
2025-04-14 14:47:31 -0300

    ac
    ac

--- countdown.js
+++ countdown.js
@@ -30,4 +30,4 @@ const countdown = (deadline, elem, finalMessage ) => {
 };
 
 
-countdown('may 04 2025 00:00:00 GMT-0300', 'clock', 'llego el dia')
+countdown('may 04 2025 13:00:00 GMT-0300', 'clock', 'llego el dia')
